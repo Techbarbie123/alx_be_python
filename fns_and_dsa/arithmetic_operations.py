a9aec13dc874375fc82a969acfc324a9325be72d
Precious Oluchi <preciousol38@gmail.com>
2024-09-27 09:22:08 +0100

    arthmetic calculator

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2  
+    elif operation == 'substract':
+        return num1 -num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "error!",  "division by zero"
+        return num1 / num2
+    else:
+        return "error!", "operation is not valid"
+    
+    
