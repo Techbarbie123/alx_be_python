b54029e220d4690f2f1ef8691635a7735556db8a
Precious Oluchi <preciousol38@gmail.com>
2024-10-05 15:59:22 +0100

    account balance

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,26 +1,17 @@
-# bank_account.py
-
 class BankAccount:
-    def __init__(self, initial_balance=0.0):
-        """Initialize the BankAccount with an optional initial balance, default is 0."""
-        self.__account_balance = initial_balance
-
+    def __init__(self, account_balance=0):
+        self.balance =account_balance
     def deposit(self, amount):
-        """Deposit a certain amount into the account."""
-        if amount > 0:
-            self.__account_balance += amount
-        else:
-            print("Deposit amount must be positive!")
-
+        self.balance += amount
+        
     def withdraw(self, amount):
-        """Withdraw a certain amount from the account if funds are sufficient."""
-        if 0 < amount <= self.__account_balance:
-            self.__account_balance -= amount
-            return True
+        can_withdraw = 0 < amount <= self.balance
+        if can_withdraw:
+           self.balance -= amount  
+           return True
         else:
             return False
-
-    def display_balance(self):
-        """Display the current account balance."""
-        print(f"Current balance: ${self.__account_balance:.2f}")
-
+    def display_balnce(self):
+        print(f"Current Balance: ${self.balance:.2f}")
+       
+     
