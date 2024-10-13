ad8a6ff9afd76a05f53fff49a675ce7f074e5b3d
Precious Oluchi <preciousol38@gmail.com>
2024-10-13 19:49:51 +0100

    hello lib

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,30 +1,35 @@
+# Base class for a Book
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+
     def __str__(self):
         return f"Book: {self.title} by {self.author}"
-        
+
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
+
     def __str__(self):
-        return f"EBook: {super().__str__()} [Ebook, {self.file_size}]KB"
-      
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
+
     def __str__(self):
-              
-       return f"PrintBook: {super().__str__()} [Print Book, {self.page_count} pages]"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
 class Library:
     def __init__(self):
-        self.books = []
+        self.books = []  
+
     def add_book(self, book):
-        if isinstance(book, Book):  
-            self.books.append(book)  
+        if isinstance(book, Book): 
+            self.books.append(book)
         else:
             print("Error: Only books can be added to the library.")
 
@@ -34,6 +39,5 @@ class Library:
         else:
             print("Listing all books in the library:")
             for book in self.books:
-                print(book)  
-        
-       
+                print(book)
+
